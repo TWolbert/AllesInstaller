67b6fd57bc9526fcc57ffd5cb51d36311fc71e74
Teun Wolbert <teunwolbert@gmail.com>
2025-01-18 00:01:17 +0100

    Testing Avalonia

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,6 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace MyLinuxApp;
 
@@ -7,5 +9,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    public void Button_Click(object sender, RoutedEventArgs args)
+    {
+        message.Text = "Hello, Avalonia!";
     }
 }
